bffbf43cf6c51498cc711bdab5adac6ea0f218e1
Sanne Strikkers <sanne.strikkers@hva.nl>
2016-06-16 16:23:55 +0200

    updated the map

--- proj/src/js/netherlands.js
+++ proj/src/js/netherlands.js
@@ -10,73 +10,106 @@ Sanne Strikkers
 11170816
 
 */
-var data = [];
-// load files into a queue for recieving data
-queue()
-.defer(d3.json, 'data/netherlands/electricity/netherlands_elek_2010.json')
-.defer(d3.json, 'data/netherlands/electricity/netherlands_elek_2012.json')
-.defer(d3.json, 'data/netherlands/electricity/netherlands_elek_2014.json')
-.defer(d3.json, 'data/netherlands/gas/netherlands_gas_2010.json')
-.defer(d3.json, 'data/netherlands/gas/netherlands_gas_2012.json')
-.defer(d3.json, 'data/netherlands/gas/netherlands_gas_2014.json')
-.await(createData);
 
-/* 
-	ChangeColor takes a path ID and a color (hex value)
-	and changes that path's fill color 
-	*/
-	function changeColor(id, color) {
-		if(document.getElementById(id) !== null) {
-			document.getElementById(id).style.fill = color;
+var svgMap = d3.select('svg');
+var div = d3.select("body").append("div").attr("class", "donut-d3-tip");
+
+var year = "2010";
+var product = "electricity";
+
+var dataList = [];
+
+// when selecting another variable, the map will be updated
+d3.selectAll("select")
+.on("change", function () {
+	year = d3.select(this).property('value');
+	choiceUpdate();
+});
+
+d3.selectAll(".radio-product")
+.on("click", function () {
+	product = d3.select(this).attr("value");
+	choiceUpdate();
+});
+
+mapUpdate("data/netherlands/electricity/netherlands_elek_2010.json");
+
+function choiceUpdate() {
+	if (product == "electricity") {
+		if (year == "2010") {
+			mapUpdate("data/netherlands/electricity/netherlands_elek_2010.json");
+			titleChange("Gemiddeld stroom verbruik in 2010");
+		} else if (year == "2012") {
+			mapUpdate("data/netherlands/electricity/netherlands_elek_2012.json");
+			titleChange("Gemiddeld stroom verbruik in 2012");
+		} else {
+			mapUpdate("data/netherlands/electricity/netherlands_elek_2014.json");
+			titleChange("Gemiddeld stroom verbruik in 2014");
+		}
+	} else {
+		if (year == "2010") {
+			mapUpdate("data/netherlands/gas/netherlands_gas_2010.json");
+			titleChange("Gemiddeld gas verbruik in 2010");
+		} else if (year == "2012") {
+			mapUpdate("data/netherlands/gas/netherlands_gas_2012.json");
+			titleChange("Gemiddeld gas verbruik in 2012");
+		} else {
+			mapUpdate("data/netherlands/gas/netherlands_gas_2014.json");
+			titleChange("Gemiddeld gas verbruik in 2014");
 		}
 	}
+}
 
+function titleChange(text) {
+	d3.select("#title-change").html(text);
+}
 
-
-/*
-	Getting data from loaded files.
-	Push the data in a public array as JSON objects.
-	*/
-	function createData(error, elek10, elek12, elek14, gas10, gas12, gas14) {
-	// error checking
-	if (error) {
-		console.log("We cannot retrieve the data.");
-		d3.select("#map-container")
-		.html("<div class='text-center'><h3>Sorry.<br><br> Wij kunnen geen data ophalen.</h3></div>");
-		throw error;
-	};
-	// for each file save the data in the public list
-	elek10.forEach(function (d) {
-		data.push({"code": d.code, "color": d.color, "community": d.community, "sjv": d.sjv});
-		// changeColor(d.code, d.color);
+function mapUpdate(file) {
+	dataList.length = 0;
+	// get data from json file and update the map with these values
+	d3.json(file, function (error, data) {
+		if (error) {
+			console.log("We cannot retrieve the data.");
+			alert("We cannot retrieve the data.");
+			throw error;
+		};
+	    // update map
+	    data.forEach(function (d) {
+	    	dataList.push({"code": d.code, "color": d.color, "community": d.community, "sjv": d.sjv});
+	    });
+	    setMap(dataList);
 	});
+}
 
-	d3.xml("src/img/nederland_gemeenten.svg", "image/svg+xml", function(error, xml) {
-  		if (error) throw error;
-  			document.getElementById("nl-map").appendChild(xml.documentElement);
-  			setTooltip(data);
+function setMap(data) {
+	var path = svgMap.selectAll("path")
+		.data(data, function(d) { return (d && d.code) || d3.select(this).attr("id"); })
+		.attr('fill', function(d){return d.color;});
+
+	if (product == "electricity") {
+		// tooltip when hovering the bars
+		path
+		.on("mousemove", function(d){
+			div.style("left", d3.event.pageX+10+"px");
+			div.style("top", d3.event.pageY-25+"px");
+			div.style("display", "inline-block");
+			div.html("<strong>" + d.community + "</strong><br>SJV: " + d.sjv + " kWh");
+		})
+		.on("mouseout", function(d){
+			div.style("display", "none");
 		});
-
+	} else {
+		// tooltip when hovering the bars
+		path
+		.on("mousemove", function(d){
+			div.style("left", d3.event.pageX+10+"px");
+			div.style("top", d3.event.pageY-25+"px");
+			div.style("display", "inline-block");
+			div.html("<strong>" + d.community + "</strong><br>SJV: " + d.sjv + " m3");
+		})
+		.on("mouseout", function(d){
+			div.style("display", "none");
+		});
+	}
 }
 
-
-function setTooltip(data) {
-	// tooltip when hovering the bars
-	var div = d3.select("body").append("div").attr("class", "donut-d3-tip");
-	console.log(d3.select("svg"));
-
-	// tooltip when hovering the bars
-	d3.selectAll("path")
-    .data(data)
-    .attr('fill', function(d){return d.color;})
-	.on("mousemove", function(d){
-		div.style("left", d3.event.pageX+10+"px");
-		div.style("top", d3.event.pageY-25+"px");
-		div.style("display", "inline-block");
-		div.html("<strong>" + d.community + "</strong><br>" + d.sjv);
-	})
-	.on("mouseout", function(d){
-		div.style("display", "none");
-	});
-
-}
